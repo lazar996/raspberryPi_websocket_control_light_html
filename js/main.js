5e5a69136bd301deb96eb7eb596da06665a7c09b
Lazar-PC <lazardjeric96@gmail.com>
2021-12-18 21:33:56 +0100

    public code!

--- js/main.js
+++ js/main.js
@@ -1,26 +1,19 @@
+// IP : change!
 var ws = new WebSocket("ws://192.168.1.5:3333");
-
+var blink = document.querySelector(".blink")
 function onSocketMessage(event) {
 			}
 	ws.onmessage = onSocketMessage;
-
 	function send() {
-		var text = document.getElementById("text").value;
-		ws.send(text);
 		}
 
-	function turnon() {
-		ws.send("on");
-		}
+function turnon() {
+	ws.send("on");
 
-	function turnoff() {
-		ws.send("off");
-		}
+	}
 
-function blink(){
-
-	
-	 
-}
+function turnoff() {
+	ws.send("off");
+	}
 
 		
